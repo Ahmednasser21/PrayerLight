42f3f115f7d938b7dd32091bd3da210a9dcd8a07
Ahmed Nasser <ahmed.nasser.11097@gmail.com>
2024-01-05 18:49:29 +0200

    All the three pre settings activities (Next and previous buttons handling)

--- app/src/main/java/com/demo1/prayerlight/AlarmSettings.kt
+++ app/src/main/java/com/demo1/prayerlight/AlarmSettings.kt
@@ -1,11 +1,19 @@
  package com.demo1.prayerlight
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.demo1.prayerlight.databinding.ActivityAlarmSettingsBinding
 
-class AlarmSettings : AppCompatActivity() {
+ class AlarmSettings : AppCompatActivity() {
+    private lateinit var binding: ActivityAlarmSettingsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_alarm_settings)
+        binding= ActivityAlarmSettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.previous.setOnClickListener{
+            val intent = Intent (this , LocationActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/demo1/prayerlight/LocationActivity.kt
+++ app/src/main/java/com/demo1/prayerlight/LocationActivity.kt
@@ -1,11 +1,23 @@
 package com.demo1.prayerlight
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.demo1.prayerlight.databinding.ActivityLocationBinding
 
 class LocationActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLocationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_location)
+        binding = ActivityLocationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.next.setOnClickListener{
+            val intent = Intent(this,AlarmSettings::class.java)
+            startActivity(intent)
+        }
+        binding.previous.setOnClickListener{
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
