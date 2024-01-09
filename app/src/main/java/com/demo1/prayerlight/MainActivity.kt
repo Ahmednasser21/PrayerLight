f2b58b6d2c3697a4487d6c9ed33f22cb5276793f
Ahmed Nasser <ahmed.nasser.11097@gmail.com>
2024-01-09 06:37:28 +0200

    All the three pre settings activities (initiate activity main background and actionbar)

--- app/src/main/java/com/demo1/prayerlight/MainActivity.kt
+++ app/src/main/java/com/demo1/prayerlight/MainActivity.kt
@@ -1,17 +1,37 @@
 package com.demo1.prayerlight
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.demo1.prayerlight.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-//        supportActionBar?.setCustomView(R.layout.action_bar)
-//        // Enable the display of the custom view
-//        supportActionBar?.setDisplayShowCustomEnabled(true)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        val signInItem = menu?.findItem(R.id.signin)
+        val signInLayout = signInItem?.actionView
+        signInLayout?.setOnClickListener {
+            Toast.makeText(this, signInItem.title, Toast.LENGTH_SHORT).show()
+        }
+
+            return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when ( item.itemId){
+            R.id.settings ->  Toast.makeText(this,item.title,Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
