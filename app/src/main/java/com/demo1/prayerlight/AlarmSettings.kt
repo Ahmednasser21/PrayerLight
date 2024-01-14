c8d2441ed2584c1f0d8b3f94dad221f3018ddf03
Ahmed Nasser <ahmed.nasser.11097@gmail.com>
2024-01-14 20:04:45 +0200

    start location services in main fragment(pre complete bottom nav view configuration)

--- app/src/main/java/com/demo1/prayerlight/AlarmSettings.kt
+++ app/src/main/java/com/demo1/prayerlight/AlarmSettings.kt
@@ -6,18 +6,18 @@ import androidx.appcompat.app.AppCompatActivity
 import com.demo1.prayerlight.databinding.ActivityAlarmSettingsBinding
 
  class AlarmSettings : AppCompatActivity() {
-    private lateinit var binding: ActivityAlarmSettingsBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding= ActivityAlarmSettingsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.previous.setOnClickListener{
-            val intent = Intent (this ,LanguageActivity::class.java )
-            startActivity(intent)
-        }
-        binding.finish.setOnClickListener{
-            val intent = Intent(this , LocationActivity::class.java)
-            startActivity(intent)
-        }
-    }
-}
+     private lateinit var binding: ActivityAlarmSettingsBinding
+     override fun onCreate(savedInstanceState: Bundle?) {
+         super.onCreate(savedInstanceState)
+         binding = ActivityAlarmSettingsBinding.inflate(layoutInflater)
+         setContentView(binding.root)
+         binding.previous.setOnClickListener {
+             val intent = Intent(this, LanguageActivity::class.java)
+             startActivity(intent)
+         }
+         binding.next.setOnClickListener {
+             val intent = Intent(this, LocationActivity::class.java)
+             startActivity(intent)
+         }
+     }
+ }
