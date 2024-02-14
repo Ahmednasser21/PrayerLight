e5adee5fd1909b8631d1afe99a9d2230868e6796
Ahmed Nasser <ahmed.nasser.11097@gmail.com>
2024-02-14 18:32:39 +0200

    till PrayerTimes(finishing spinner )

--- app/src/main/java/com/demo1/prayerlight/PrayerTimes.kt
+++ app/src/main/java/com/demo1/prayerlight/PrayerTimes.kt
@@ -4,55 +4,67 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.demo1.prayerlight.databinding.FragmentPrayerTimesBinding
+import java.time.LocalDate
+import java.time.chrono.HijrahDate
+import java.time.format.DateTimeFormatter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [PrayerTimes.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PrayerTimes : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentPrayerTimesBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_prayer_times, container, false)
+    ): View {
+        binding = FragmentPrayerTimesBinding.inflate(inflater,container,false)
+//        ======= Gregorian date =============
+        val dateGregorian = LocalDate.now()
+        val formatterGregorian = DateTimeFormatter.ofPattern("MMMM")
+        val formattedGregorianDate = dateGregorian.format(formatterGregorian)
+//         ========= Hijrah date ============
+        val hijrahDate = HijrahDate.now()
+        val formatterHijrah = DateTimeFormatter.ofPattern("MMMM")
+        val formattedHijriDate = hijrahDate.format(formatterHijrah)
+//  ======== spinner handling ===========
+        val spinner = binding.spinner
+        ArrayAdapter.createFromResource(requireContext(),R.array.spinner,R.layout.spinner_item).
+            also { adapter->
+                adapter.setDropDownViewResource(R.layout.spinner_layout)
+                spinner.adapter = adapter
+            }
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+              val textView = view as TextView
+
+               when (position){
+
+                    0-> textView.text = formattedHijriDate
+                    1-> textView.text =formattedGregorianDate
+              }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PrayerTimes.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PrayerTimes().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
